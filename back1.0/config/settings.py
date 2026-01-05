81c725f10e1a41b3f36950f47283472cbbfd7125
Lfffy <1120935704@qq.com>
2026-01-05 16:31:26 +0800

    切换数据库为 MySQL

--- back1.0/config/settings.py
+++ back1.0/config/settings.py
@@ -2,6 +2,10 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import pymysql
+
+# Use pymysql as MySQL driver
+pymysql.install_as_MySQLdb()
 
 # Load environment variables
 load_dotenv()
@@ -73,8 +77,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'codebook'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'",
+        },
     }
 }
 
